f0341ade760494a50af30927506bd9a978189903
Sergey Zlygostev <zlygostev.sergey1983@gmail.com>
2024-01-23 12:45:23 +0400

    fix: 06-build-page add assets & build html

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,3 +1,4 @@
+const {error} = require('console');
 const fs = require('fs');
 const path = require('path');
 const promis = fs.promises;
@@ -5,29 +6,34 @@ const styles = path.join(__dirname, 'styles');
 const dist = path.join(__dirname, 'project-dist');
 const template = path.join(__dirname, 'template.html');
 const components = path.join(__dirname, 'components');
-const componentsObj = {};
+const assets = path.join(__dirname, 'assets');
+const distAssets = path.join(dist, 'assets');
 
 makeDist();
+makeDistAssets();
+copyImg();
+copyFont();
+copySvg();
 createIndexFile();
 combineStyles();
 
 function makeDist() {
   fs.mkdir(dist, { recursive: true }, (error) => {
-    if (error) {
-      throw new Error('Error: unable to create directory');
-    } else {
-      console.log('Success: create directory');
-    }
+    if (error) throw error;
+  });
+}
+
+function makeDistAssets() {
+  fs.mkdir(path.join(dist, 'assets'), { recursive: true }, (error) => {
+    if (error) throw error;
   });
 }
 
 function createIndexFile() {
-  const output = fs.createWriteStream(path.join(dist, 'index.html'));
-  const input = fs.createReadStream(template, 'utf-8');
-  let content = [];
+  let content = '';
 
-  input.on('data', (data) => {
-    let inputData = data.toString();
+  fs.readFile(template, 'utf-8', (error, data) => {
+    content = content + data.toString();
     fs.readdir(components, (error, data) => {
       if (error) throw error;
       data.forEach((el) => {
@@ -38,22 +44,14 @@ function createIndexFile() {
         if (elExt !== '.html') return;
         const elNameWithoutExt = elName.replace(elExt, '');
         const alias = `{{${elNameWithoutExt}}}`;
-        const elContent = fs.createReadStream(path.join(components, elName), 'utf-8');
 
-        if (inputData.indexOf(alias) >= 0) {
-          elContent
-            .on('data', (elData) => {
-              let item = {
-                [elNameWithoutExt]: elData.toString(),
-              };
-              content.push(item);
-            })
-            .on('end', () => {
-              return content;
-            });
-        }
-
-        console.log(content);
+        fs.readFile(path.join(components, elName), 'utf-8', (error, elData) => {
+          if (error) throw error;
+          content = content.replace(alias, elData);
+          fs.writeFile(path.join(dist, 'index.html'), content, (error) => {
+            if (error) throw error;
+          });
+        });
       });
     });
   });
@@ -71,6 +69,54 @@ function combineStyles() {
       const stylesContent = fs.createReadStream(path.join(styles, elName));
       stylesContent.on('data', (data) => {
         output.write(data);
+      });
+    });
+  });
+}
+
+function copyFont() {
+  fs.mkdir(path.join(distAssets, 'fonts'), { recursive: true }, (error) => {
+    if (error) throw error;
+  });
+
+  fs.readdir(path.join(assets, 'fonts'), (error, data) => {
+    if (error) throw error;
+    data.forEach((el) => {
+      const elPath = path.join(assets, 'fonts', el);
+      fs.copyFile(elPath, path.join(distAssets, 'fonts', el), (error) => {
+        if (error) throw error;
+      });
+    });
+  });
+}
+
+function copyImg() {
+  fs.mkdir(path.join(distAssets, 'img'), { recursive: true }, (error) => {
+    if (error) throw error;
+  });
+
+  fs.readdir(path.join(assets, 'img'), (error, data) => {
+    if (error) throw error;
+    data.forEach((el) => {
+      const elPath = path.join(assets, 'img', el);
+      fs.copyFile(elPath, path.join(distAssets, 'img', el), (error) => {
+        if (error) throw error;
+      });
+    });
+  });
+}
+
+function copySvg() {
+  fs.mkdir(path.join(distAssets, 'svg'), { recursive: true }, (error) => {
+    if (error) throw error;
+  });
+
+  fs.readdir(path.join(assets, 'svg'), (error, data) => {
+    if (error) throw error;
+    data.forEach((el) => {
+      const elPath = path.join(assets, 'svg', el);
+      fs.copyFile(elPath, path.join(distAssets, 'svg', el), (error) => {
+        if (error) throw error;
       });
     });
   });
